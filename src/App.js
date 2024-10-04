3f1b4254a6495b6e0b1e0a7bff7b3577a65b7d7a
Natasha Zywczyk <g00415696@atu.ie>
2024-10-04 09:00:10 +0000

    Exercise 5 Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
@@ -9,9 +11,16 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path ="/home" element = {<Content />} />
+          <Route path = "/read" element = {<h1>Read Component</h1>} />
+          <Route path = "/create" element = {<h1>Create Component</h1>} />
+        </Routes>
+        <Header />
+        <Footer />
+      </Router>
     </div>
   );
 }
